5a28121f835a24cd1bac6fc0c9a29be515866d76
wigswag <ton-email@example.com>
2024-10-27 00:02:47 +0200

    Enregistrement des modifications et mise à jour du projet

--- test.dart
+++ test.dart
@@ -1,4 +1,28 @@
-void main() {
-  var variable = "Hello"; // Exemple de chaîne
+import 'package:url_launcher/url_launcher.dart';
+
+void main() async {
+  // Exemple de chaîne
+  var variable = "Hello"; 
   print(variable.runtimeType); // Devrait afficher "String"
+
+  // Exemple de réponse d'API (remplacez-le par votre code réel)
+  var response = {
+    'wave_launch_url': "https://pay.wave.com/c/cos-1sed01k08128c?a=1000&c=XOF&m=Fred%20Trade%20Senegal"
+  };
+
+  // Vérifiez le type de la variable
+  String waveLaunchUrl = response['wave_launch_url'];
+  print('Type de waveLaunchUrl: ${waveLaunchUrl.runtimeType}'); // Devrait afficher "String"
+
+  // Lancer la page de paiement
+  await launchPaymentPage(waveLaunchUrl);
+}
+
+// Fonction pour lancer l'URL
+Future<void> launchPaymentPage(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
 }

33f1e8897b517a8d57750449bde99593139224ab
wigswag <ton-email@example.com>
2024-11-23 19:53:02 +0000

    Ajout de la route /v1/payment/session et configuration fonctionnelle de Nginx

--- wave_api_directory/index.js
+++ wave_api_directory/index.js
@@ -1,11 +1,14 @@
+// Importer Express
 const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello from Wave API!');
+// Route GET pour l'API Wave
+app.get('/api/get_payment_info', (req, res) => {
+    res.send({ message: 'Hello from Wave API!' });
 });
 
+// Démarrer le serveur
 app.listen(port, () => {
     console.log(`Wave API listening at http://localhost:${port}`);
 });
